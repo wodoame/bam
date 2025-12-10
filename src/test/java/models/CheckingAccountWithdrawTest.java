4a3260edca0253c50a36ab53535ce8f60a56ea8e
Wodoame <bmwodoame@gmail.com>
2025-12-10 11:23:12 +0000

    Add tests to check withdrawals

--- src/test/java/models/CheckingAccountWithdrawTest.java
+++ src/test/java/models/CheckingAccountWithdrawTest.java
@@ -34,13 +34,32 @@ class CheckingAccountWithdrawTest extends AccountTestBase {
         CheckingAccount account = new CheckingAccount(regularCustomer, 0);
 
         assertThrows(OverdraftExceededException.class, () -> account.withdraw(1100));
+        assertEquals(0, account.getBalance());
     }
 
     @Test
     void withdrawRejectsNonPositiveAmount() {
         CheckingAccount account = new CheckingAccount(regularCustomer, 500);
         assertThrows(InvalidWithdrawalAmountException.class, () -> account.withdraw(0));
+        assertEquals(500, account.getBalance());
         assertThrows(InvalidWithdrawalAmountException.class, () -> account.withdraw(-50));
+        assertEquals(500, account.getBalance());
+    }
+
+    @Test
+    void withdrawExactBalanceLeavesZero() {
+        CheckingAccount account = new CheckingAccount(regularCustomer, 500);
+        boolean result = account.withdraw(500);
+        assertTrue(result);
+        assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    void withdrawExactOverdraftLimitLeavesNegativeLimit() {
+        CheckingAccount account = new CheckingAccount(regularCustomer, 0);
+        // Overdraft limit is 1000. Balance 0 - 1000 = -1000
+        boolean result = account.withdraw(CheckingAccount.OVERDRAFT_LIMIT);
+        assertTrue(result);
+        assertEquals(-CheckingAccount.OVERDRAFT_LIMIT, account.getBalance());
     }
 }
-
